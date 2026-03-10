a4fe753837185ade9d617d9027be70d6520d2a4b
Gilberto <gilbertorm.ramos@gmail.com>
2026-03-10 15:28:05 +0100

    fixing cache prisma.js

--- netlify/functions/_shared/prisma.js
+++ netlify/functions/_shared/prisma.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 
 // Hardcoded database URL - always use this value
-process.env.DATABASE_URL = 'postgres://6305e14dbb95f6bef6388277bec34f2fe0fe5fae760e09f9e92001fea108a2ba:sk_BBvvaIYQ2Nc3-MKNFgDiW@db.prisma.io:5432/postgres?sslmode=require';
+process.env.DATABASE_URL = 'postgres://6305e14dbb95f6bef6388277bec34f2fe0fe5fae760e09f9e92001fea108a2ba:sk_BBvvaIYQ2Nc3-MKNFgDiW@db.prisma.io:5432/postgres?sslmode=require&statement_cache_size=0';
 
 let prisma;
 
